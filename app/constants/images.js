ccd1ecd2965109d57ba9fd8366cb0513bda2995f
Samir Omer <samiromer2@yahoo.com>
2023-05-27 04:52:36 +0300

    keep yup and follow up

---
+++ app/constants/images.js
@@ -0,0 +1,7 @@
+export const onboarding1 = require("../assets/images/onborading-1.png");
+export const onboarding2 = require("../assets/images/onborading-2.png");
+export const onboarding3 = require("../assets/images/onborading-3.png");
+
+export default {
+    onboarding1,onboarding2,onboarding3
+}
